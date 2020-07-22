7bdab7ccbb96ab03b086f8763b39e25774b5bd8f
Ipsita Bisht <ipsitabisht@gmail.com>
2020-07-22 16:39:10 -0700

    First Commit

--- AlarmClock/ViewController.swift
+++ AlarmClock/ViewController.swift
@@ -1,18 +1,18 @@
-//
-//  ViewController.swift
-//  AlarmClock
-//
-//  Created by Not Tanu on 7/22/20.
-//  Copyright © 2020 Ipsita. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var addSecButton: UIButton!
+    @IBOutlet weak var minusSecButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         
     }
 
 
