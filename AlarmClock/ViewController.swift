34be42ef256c408427163bbef5a8c335d229bd59
Ipsita Bisht <ipsitabisht@gmail.com>
2020-07-22 17:39:27 -0700

    tried setting up timer

--- AlarmClock/ViewController.swift
+++ AlarmClock/ViewController.swift
@@ -4,17 +4,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    
     @IBOutlet weak var timerLabel: UILabel!
-    @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var pauseButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var addSecButton: UIButton!
-    @IBOutlet weak var minusSecButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    var timer = Timer()
+    var time = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
          
     }
 
+    /*@IBAction func startButton(_ sender: Any) {
+        //Make sure there arent any other timers running
+        timer.invalidate()
+        //Create the timer
+        timer = Timer.scheduledTimer(timeInterval:  1 , target: self ,
+        
+    }*/
+    
 
+    @IBAction func startTime(_ sender: UIButton) {
+        timer.invalidate()
+        if let newTime = textField.text{
+            timerLabel.text = newTime
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(ViewController.timeClass), userInfo: nil, repeats: true)
+      
+       
+        
+        
+    }
+    
+    @objc func timeClass(){
+        timerLabel.text =
+    }
+    
 }
-
